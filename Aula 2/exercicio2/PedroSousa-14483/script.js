8f41a6e03de16ced7bbef1c8b9a563551b696f95
pedromcsousa <pedrosousa1@sapo.pt>
2020-03-02 14:45:55 +0000

    Aula02-PedroSousa-14483

--- Aula 2/exercicio2/PedroSousa-14483/script.js
+++ Aula 2/exercicio2/PedroSousa-14483/script.js
@@ -8,22 +8,24 @@ function adicionaNumero() {
   var numeros = document.getElementById("numeros");
   numerosArray.push(parseInt(numero.value));
   var resultado = "Adicionados: ";
-  for(i = 0; i < numerosArray.length; i++){
-    if(i != 0){
+  for (i = 0; i < numerosArray.length; i++) {
+    if (i != 0) {
       resultado += ",";
     }
     resultado += " " + numerosArray[i];
   }
   numeros.innerHTML = resultado;
   numero.value = "";
-  document.getElementById("calcular").disabled = false;
+  if (numerosArray.length >= 5) {
+    document.getElementById("calcular").disabled = false;
+  }
 }
 
-function calcularMaiorNumero(){
+function calcularMaiorNumero() {
   var maior = numerosArray[0];
-  var maiorNumero = document.getElementById('maiorNumero');
-  for(i = 0; i < numerosArray.length; i++){
-    if(numerosArray[i] >= maior){
+  var maiorNumero = document.getElementById("maiorNumero");
+  for (i = 0; i < numerosArray.length; i++) {
+    if (numerosArray[i] >= maior) {
       maior = numerosArray[i];
     }
   }
