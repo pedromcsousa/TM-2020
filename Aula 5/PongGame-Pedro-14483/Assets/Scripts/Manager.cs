58e4d796b772e8309b1e4450305ae7108a9a9564
pedromcsousa <pedrosousa1@sapo.pt>
2020-03-23 15:25:15 +0000

    PedroSousa-14483

---
+++ Aula 5/PongGame-Pedro-14483/Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Manager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Score UI")]
+    public GameObject playerScoreText;
+
+    private int playerScore;
+
+    public void PlayerScored()
+    {
+        playerScore++;
+        playerScoreText.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+        ball.GetComponent<Ball>().increaseSpeed();
+    }
+
+    public void Reset()
+    {
+        playerScore = 0;
+        playerScoreText.GetComponent<TextMeshProUGUI>().text = playerScore.ToString();
+    }
+}
